07cf44565da57d72428609947205f92cddfe0d9a
vani-lu <67897311+vani-lu@users.noreply.github.com>
2022-04-19 09:58:36 +0800

    Save exported map files

--- Assets/Scripts/Replay/MapJsonExport.cs
+++ Assets/Scripts/Replay/MapJsonExport.cs
@@ -1,3 +1,5 @@
+using System.IO;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEditor;
 using Gfen.Game.Config;
@@ -50,6 +52,9 @@ namespace Gfen.Game.Map
             if (GUILayout.Button("Export Directory", GUILayout.Width(150)))
             {
                 EditorPrefs.SetString(exportDirectoryKey, m_exportDirectory);
+                if (!Directory.Exists(m_exportDirectory)){
+                    Directory.CreateDirectory(m_exportDirectory);
+                }
             }
             GUILayout.EndHorizontal();
 
@@ -67,26 +72,41 @@ namespace Gfen.Game.Map
             EditorGUILayout.Separator();
 
             GUILayout.BeginHorizontal();
-
             m_isParsable = false;
             m_isParsable = int.TryParse(EditorGUILayout.TextField(m_chapter.ToString()), out m_chapter);
             m_isParsable = m_isParsable && int.TryParse(EditorGUILayout.TextField(m_level.ToString()), out m_level);
             GUILayout.EndHorizontal();
+
+            if (m_isParsable){
+                GUI.enabled = true;
+            }
+            else {
+                ShowTip("请输入正整数");
+            }
+
             if (GUILayout.Button("Export Json", GUILayout.Width(150)))
             {
                 EditorPrefs.SetString(configPathKey, m_configPath);
-                m_mapconfig = m_gameconfig.chapterConfigs[m_chapter].levelConfigs[m_level].map;
+                m_mapconfig = m_gameconfig.chapterConfigs[m_chapter-1].levelConfigs[m_level-1].map;
                 ExportMap();
             }
 
-            GUI.enabled = true;
 
         }
 
-
         private void ExportMap()
         {
-            Debug.Log(JsonUtility.ToJson(m_mapconfig));
+            // Debug.Log(JsonUtility.ToJson(m_mapconfig));
+            string content = JsonUtility.ToJson(m_mapconfig, true);
+            string fileDirectory = m_exportDirectory + "map_chp_" + m_chapter.ToString() 
+                                    + "_lvl_" + m_level.ToString() + ".json";
+
+            if (!File.Exists(fileDirectory)){
+                File.Create(fileDirectory).Dispose();
+            }
+
+            using StreamWriter file = new StreamWriter(fileDirectory, append: false);
+            file.WriteLine(content);
         }
 
         // 错误处理
@@ -96,4 +116,3 @@ namespace Gfen.Game.Map
         }
     }
 }
-
